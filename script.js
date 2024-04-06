11caef8ff3c72362dbd0b5874258395754398c25
CiriloRibeiro <cirilo.ribeiroluiz@gmail.com>
2024-04-06 14:25:39 -0300

    finished isvalid year func

--- script.js
+++ script.js
@@ -17,16 +17,19 @@ function validateInput(input, min, max) {
         input.style.border = border + warningColor;
         labelForInput.style.color = warningColor;
         return false; // Invalid input
+    } else if (value.length >= 1) {
+        input.style.border = border + usualColor;
+        labelForInput.style.color = usualColor;
+        return true;
     } else {
         input.style.border = border + usualColor;
         labelForInput.style.color = usualColor;
-        return true; // Valid input
+        return;
     }
 }
 
 dayInput.addEventListener('input', () => {
-    if (!validateInput(dayInput, 1, 31)) {
-        
+    if (!validateInput(dayInput, 1, 31) && dayInput.value.trim().length > 0) {
         createErrorMessage(dayInput, 'Must be a valid day');
     } else {
         removeErrorMessage(dayInput);
@@ -34,7 +37,7 @@ dayInput.addEventListener('input', () => {
 });
 
 monthInput.addEventListener('input', () => {
-    if (!validateInput(monthInput, 1, 12)) {
+    if (!validateInput(monthInput, 1, 12) && monthInput.value.trim().length > 0) {
         createErrorMessage(monthInput, 'Must be a valid month');
     } else {
         removeErrorMessage(monthInput);
@@ -42,28 +45,50 @@ monthInput.addEventListener('input', () => {
 });
 
 yearInput.addEventListener('input', () => {
-    if (!validateInput(yearInput, 1, currentYear)) {
+    if (!validateInput(yearInput, 1, currentYear) && yearInput.value.trim().length > 0) {
         createErrorMessage(yearInput, 'Must be a valid year');
     } else {
         removeErrorMessage(yearInput);
     }
 });
 
+function isDateValid(dateStr) {
+    return isFinite(new Date(dateStr));
+  }
+
 button.addEventListener('click', () => {
-    const isValid = validateInput(dayInput, 1, 31) && validateInput(monthInput, 1, 12) && validateInput(yearInput, 0, 2024);
-    if (isValid) {
+    const isValid = validateInput(dayInput, 1, 31) && validateInput(monthInput, 1, 12) && validateInput(yearInput, 1, 2024);
+    const fullDate = `${yearInput.value}-${monthInput.value}-${dayInput.value}`;
+    
+    let date = moment(fullDate, 'YYYY-MM-DD');
+    isDateValid = date.isValid();
+    
+
+    if (isValid && isDateValid) {
         alert('All inputs are valid and not empty');
-    } else {
+    } else if (!isValid) {
         if (!validateInput(dayInput, 1, 31)) {
             createErrorMessage(dayInput, 'This field is required');
-        }   
+        }
         if (!validateInput(monthInput, 1, 12)) {
             createErrorMessage(monthInput, 'This field is required');
         }   
         if (!validateInput(yearInput, 0, currentYear)) {
             createErrorMessage(yearInput, 'This field is required');
-        }   
-       
+        }     
+    } else if (!isDateValid) {    
+        const labelForDay = document.querySelector(`label[for="${dayInput.id}"]`);
+        const labelForYear = document.querySelector(`label[for="${monthInput.id}"]`);
+        const labelForMonth = document.querySelector(`label[for="${yearInput.id}"]`);
+        createErrorMessage(dayInput, 'Must be a valid date');
+        createErrorMessage(monthInput, 'Must be a valid date');
+        createErrorMessage(yearInput, 'Must be a valid date');
+        dayInput.style.border = border + warningColor;
+        labelForDay.style.color = warningColor;
+        monthInput.style.border = border + warningColor;
+        labelForMonth.style.color = warningColor;
+        yearInput.style.border = border + warningColor;
+        labelForYear.style.color = warningColor;
     }
 });
 
